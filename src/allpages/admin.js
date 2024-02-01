fac2e5912f5c7cb1b41f9dac18b3225b69c2f0c2
Poomlak <mrpoomkakmak@hotmail.com>
2024-02-02 00:08:41 +0700

    Add roll,package , change admin

--- src/allpages/admin.js
+++ src/allpages/admin.js
@@ -23,28 +23,29 @@ function Admin() {
           <div className="sign-in">หน้าหลัก</div>
         </div>
       </div>
-      <div>
-        <div className="box1">บทบาท</div>
-        <div className="box2">ตรวจสอบความถูกต้อง</div>
-        <div className="box3">
-          <text>ชื่อ :</text>
+      <div className="whitebox">
+        <div>
+          <div id="box1">บทบาท</div>
+          <div id="box2">ตรวจสอบความถูกต้อง</div>
+          <div id="box3">
+            <text>ชื่อ :</text>
+          </div>
+          <div id="box4">
+            <text>สาขา : </text>
+          </div>
+          <div id="box5">
+            <text>คณะ : </text>
+          </div>
+          <div id="box6">
+            <text>เมล : </text>
+          </div>
+          <div id="box8">
+            <text>โทร : </text>
+          </div>
+          <div id="box9">SIGN OUT</div>
+          <div id="circle"></div>
         </div>
-        <div className="box4">
-          <text>สาขา : </text>
-        </div>
-        <div className="box5">
-          <text>คณะ : </text>
-        </div>
-        <div className="box6">
-          <text>เมล : </text>
-        </div>
-        <div className="box8">
-          <text>โทร : </text>
-        </div>
-        <div className="box9">SIGN OUT</div>
-        <div className="circle"></div>
       </div>
-      <div className="whitebox"></div>
     </div>
   );
 }
