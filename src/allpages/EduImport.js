8bef6a929a9b61cb41d369a9850173195926e67a
nanfha <29418nanfha@gmail.com>
2024-02-06 23:32:36 +0700

    Import pop up

---
+++ src/allpages/EduImport.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../allstyles/EduImport.css';
+
+function EduImport() {
+    const handleFileSelect = () => {
+        const fileInput = document.getElementById('fileInput');
+        fileInput.click();
+    };
+
+    const handleDragOver = (event) => {
+        event.preventDefault();
+    };
+
+    const handleDragEnter = (event) => {
+        event.preventDefault();
+        event.target.classList.add('drag-over');
+    };
+
+    const handleDragLeave = (event) => {
+        event.preventDefault();
+        event.target.classList.remove('drag-over');
+    };
+
+    const handleDrop = (event) => {
+        event.preventDefault();
+        const files = event.dataTransfer.files;
+        console.log(files); 
+        event.target.classList.remove('drag-over');
+    };
+
+    return (
+        <div className='allbox'>
+            <label className='box1' 
+                   onDragOver={handleDragOver}
+                   onDragEnter={handleDragEnter}
+                   onDragLeave={handleDragLeave}
+                   onDrop={handleDrop}></label>
+            <label className='box2' onClick={handleFileSelect}>เลือกไฟล์</label>
+            <input type="file" id="fileInput" style={{ display: 'none' }} accept=".xlsx" />
+            <label className='box3'>ลากไฟล์ลงมา หรือ คลิกเพื่อเลือกไฟล์</label>
+            <label className='box4'>import รายวิชา เป็นนามสกุลไฟล์ .xlsx</label>
+            <span className="carbon--close-filled"></span>
+            <span className="pajamas--import"></span>
+            <label className='whitebox'></label>
+        </div>
+    );
+}
+
+export default EduImport;
