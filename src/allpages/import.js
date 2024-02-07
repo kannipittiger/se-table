623837cf519569efb53bc02e5cca85fdc0891015
kannipit <kannipi.p@ku.th>
2024-02-07 21:23:45 +0700

    import excel

--- src/allpages/import.js
+++ src/allpages/import.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import logo from "../allstyles/englogo.png";
 //import { CiSearch } from "react-icons/ci";
 import { useNavigate } from "react-router-dom";
+import * as xlsx from "xlsx";
 import "../allstyles/import.css";
 
 function Import() {
   const navigate = useNavigate();
+
+  const [data, setData] = useState([]);
+
   const goHome = () => {
     navigate("/");
   };
+
+  /*const readExcel = async(e) => {
+    const file = e.target.file[0];
+    const data = await file.arrayBuffer(file);
+    const excelfile = xlsx.read(data);
+    const excelsheet = excelfile.Sheets[excelfile.SheetNames[0]];
+    const exceljson = xlsx.utils.sheet_to_json(excelsheet);
+    console.log(exceljson);
+  }*/
+
+  const handleFileUpload = (e) => {
+    
+      const reader = new FileReader();
+      reader.readAsBinaryString(e.target.files[0]);
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = xlsx.read(data, { type: "binary" });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const parsedData = xlsx.utils.sheet_to_json(sheet);
+        setData(parsedData);
+        console.log(data);
+      };
+    
+  }
+  
 
   return (
     <div className="allbox">
@@ -34,15 +64,36 @@ function Import() {
       </div>
       <div className="whitebox">
         <div id="boxDownload">Download Excel</div>
-        <div id="boxImport">Import</div>
+        <label id="boxImport">
+          <input type="file" title="Import" style={{ display:"none" }} accept=".xlsx,.xls" onChange={handleFileUpload} />
+          Import
+        </label>
+       
         <div>
           <label className="textรหัส">รหัสวิชา</label>
           <label className="textวิชา">ชื่อวิชา</label>
           <label className="textหน่วยกิต">หน่วยกิต</label>
           <label className="textบังคับ">บังคับ/เลือก</label>
         </div>
-        <div class="grid-container">
-          <div>
+        
+          {data.length > 0 && (
+            <div class=" scroll">
+              {data.map((row,index)=>(
+                <div key={index} className="renderimport">
+                  <div style={{width:50,height:50,border:'1px solid black',margin:'2px',borderRadius:100}}  >5</div>
+                  <div style={{flex:10,border:'1px solid black',margin:'2px'}}>{row['รหัสวิชา']}</div>
+                  <div style={{flex:10,border:'1px solid black',margin:'2px'}}>{row['ชื่อวิชา']}</div>
+                  <div style={{flex:6,border:'1px solid black',margin:'2px'}}>{row['หน่วยกิต']}</div>
+                  <div style={{flex:5,border:'1px solid black',margin:'2px'}}>{row['หมวดวิชา']}</div>
+                  {/* <div id="" style={{border:'1px solid black'}} key={index}>{row['รหัสวิชา']}</div>
+                  <div id="" key={index}>{row['ชื่อวิชา']}</div>
+                  <div id="" key={index}>{row['หน่วยกิต']}</div>
+                  <div id="" key={index}>{row['หมวดวิชา']}</div> */}
+                </div>
+              ))}
+            </div>
+          )}
+          {/* <div>
             <div className="circle1"></div>
             <div className="circle2"></div>
             <div className="circle3"></div>
@@ -66,8 +117,7 @@ function Import() {
             <label id="boxบังคับ">บังคับ</label>
             <label id="boxบังคับ2">บังคับ</label>
             <label id="boxบังคับ3">บังคับ</label>
-          </div>
-        </div>
+          </div> */}
         <label id="boxclear">เคลียร์</label>
         <label id="boxสำเร็จ">สำเร็จ</label>
       </div>
