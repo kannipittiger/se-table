77002a923ba529b9bbd02a76cde1fbc0db098968
Poomlak <mrpoomkakmak@hotmail.com>
2024-02-08 22:12:54 +0700

    Update import

--- src/allpages/import.js
+++ src/allpages/import.js
@@ -1,10 +1,10 @@
-import React,{ useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../allstyles/englogo.png";
 //import { CiSearch } from "react-icons/ci";
 import { useNavigate } from "react-router-dom";
-import {read,utils,writeFile} from "xlsx";
-import Papa from 'papaparse';
+import { read, utils, writeFile } from "xlsx";
 import "../allstyles/import.css";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 function Import() {
   const navigate = useNavigate();
@@ -17,33 +17,35 @@ function Import() {
     navigate("/");
   };
 
-
-  const handleImport = $event => {
+  const handleImport = ($event) => {
     const files = $event.target.files;
-    if(files.length) {
+    if (files.length) {
       const file = files[0];
       const reader = new FileReader();
-      reader.onload = event => {
+      reader.onload = (event) => {
         const wb = read(event.target.result);
         const sheet = wb.SheetNames;
 
-        if(sheet.length) {
+        if (sheet.length) {
           const rows = utils.sheet_to_json(wb.Sheets[sheet[0]]);
           setData(rows);
         }
-      }
+      };
       reader.readAsArrayBuffer(file);
     }
-  }
+  };
 
   const muad = (row) => {
-    if(row.muad == 0){
+    if (row.muad == 0) {
       return <div>บังคับ</div>;
-      
-    }else if(row.muad == 1){
+    } else if (row.muad == 1) {
       return <div>เลือก</div>;
     }
-  }
+  };
+  const handleButtonClick = () => {
+    window.location.href =
+      "https://drive.usercontent.google.com/download?id=1gl95LK1fOAk47hvNhAqm9MM0h-oFm2CX&export=download&authuser=2&confirm=t&uuid=e054fd4f-0772-4908-b486-dd37fc01cb9c&at=APZUnTVNmQ8_oiHQx4c5YGdcwIj5:1707400406705"; // เปลี่ยน URL เป็น URL ที่คุณต้องการ
+  };
 
   // const handleFileUpload = (e) => {
   //     const reader = new FileReader();
@@ -57,9 +59,8 @@ function Import() {
   //       setData(parsedData);
   //       console.log(data);
   //     };
-    
+
   // }
-  
 
   return (
     <div className="allbox">
@@ -67,54 +68,84 @@ function Import() {
         <img src={logo} className="imglogo" alt="logo"></img>
         <div className="kubar">
           <div className="">
-            <div className="thai-ku">
+            <div className="thai_ku">
               มหาวิทยาลัยเกษตรศาสตร์ วิทยาเขตศรีราชา
             </div>
-            <div className="english-ku">
-              Kasetsart university sriracha campus
+            <div className="english_ku">
+              Kasetsart University Sriracha Campus
             </div>
           </div>
           <div />
         </div>
-        <div className="menu-bar">
-          <div className="home-button">sign in</div>
-          <div className="sign-in" onClick={goHome}>
+        <div className="menu_bar">
+          <div className="sign_in" onClick={goHome}>
             หน้าหลัก
           </div>
+          <div className="home_button">sign in</div>
+          {/* <div className="sign_in" onClick={goHome}>
+            หน้าหลัก
+          </div> */}
         </div>
       </div>
       <div className="whitebox">
-        <div id="boxDownload">Download Excel</div>
+        <div id="boxDownload" onClick={handleButtonClick}>
+          Download Excel
+        </div>
         <label id="boxImport">
-          <input type="file" title="Import" style={{ display:"none" }} accept=".csv" onChange={handleImport} />
+          <input
+            type="file"
+            title="Import"
+            style={{ display: "none" }}
+            accept=".csv"
+            onChange={handleImport}
+          />
           Import
         </label>
-       
+
         <div>
           <label className="textรหัส">รหัสวิชา</label>
           <label className="textวิชา">ชื่อวิชา</label>
           <label className="textหน่วยกิต">หน่วยกิต</label>
           <label className="textบังคับ">บังคับ/เลือก</label>
         </div>
-        
-          {data.length > 0 && (
-            <div class=" scroll">
-              {data.map((row,index)=>(
-                <div key={index} className="renderimport">
-                  <div style={{width:50,height:50,border:'1px solid black',margin:'2px',borderRadius:100}}  >5</div>
-                  <div style={{flex:10,border:'1px solid black',margin:'2px'}}>{row.code}</div>
-                  <div style={{flex:10,border:'1px solid black',margin:'2px'}}>{row.name}</div>
-                  <div style={{flex:6,border:'1px solid black',margin:'2px'}}>{row.point}</div>
-                  <div style={{flex:5,border:'1px solid black',margin:'2px'}}>{muad(row)}</div>
-                  {/* <div id="" style={{border:'1px solid black'}} key={index}>{row['รหัสวิชา']}</div>
+
+        {data.length > 0 && (
+          <div class=" scroll">
+            {data.map((row, index) => (
+              <div key={index} className="renderimport">
+                <div
+                  id="boxรหัส"
+                  // style={{ flex: 10, border: "2px solid black", margin: "2px" }}
+                >
+                  {row.code}
+                </div>
+                <div
+                  id="boxวิชา"
+                  // style={{ flex: 10, border: "2px solid black", margin: "2px" }}
+                >
+                  {row.name}
+                </div>
+                <div
+                  id="boxหน่วยกิต"
+                  // style={{ flex: 6, border: "2px solid black", margin: "2px" }}
+                >
+                  {row.point}
+                </div>
+                <div
+                  id="boxบังคับ"
+                  // style={{ flex: 5, border: "2px solid black", margin: "2px" }}
+                >
+                  {muad(row)}
+                </div>
+                {/* <div id="" style={{border:'1px solid black'}} key={index}>{row['รหัสวิชา']}</div>
                   <div id="" key={index}>{row['ชื่อวิชา']}</div>
                   <div id="" key={index}>{row['หน่วยกิต']}</div>
                   <div id="" key={index}>{row['หมวดวิชา']}</div> */}
-                </div>
-              ))}
-            </div>
-          )}
-          {/* <div>
+              </div>
+            ))}
+          </div>
+        )}
+        {/* <div>
             <div className="circle1"></div>
             <div className="circle2"></div>
             <div className="circle3"></div>
