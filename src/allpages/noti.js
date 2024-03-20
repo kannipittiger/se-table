7e057264adca494f26bd83c4cea60983c6af8d78
example <example@gmail.com>
2024-03-21 00:24:04 +0700

    full noti

--- src/allpages/noti.js
+++ src/allpages/noti.js
@@ -1,41 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../allstyles/englogo.png'
 import '../allstyles/noti.css'
-import { IoMdCloseCircle} from "react-icons/io";
+import { IoMdCloseCircle } from "react-icons/io";
+import axios from 'axios';
 
-function Noti({setShow}) 
-{
-    return(
+function Noti({ setShow }) {
+    const [dataNotifi, setDataNotifi] = useState([]);
+    const [reload, setReload] = useState(false);
+    useEffect(() => {
+        const getapi = async () => {
+            try {
+                const dataNotifiapi = await axios.get("http://localhost:5000/notification");
+                const data = dataNotifiapi.data;
+                setDataNotifi(data)
+                console.log(data);
+            } catch (err) {
+                alert(err.response.data);
+            }
+
+        }
+        getapi()
+    }, [reload])
+    const delete_one = async (id) => {
+        try {
+            const dataNotifiapi = await axios.delete("http://localhost:5000/deletenotifi/" + id);
+            const data = dataNotifiapi.data;
+            setReload(!reload);
+        } catch (err) {
+            alert(err.response.data);
+        }
+    }
+    const delete_all = async () => {
+        dataNotifi.map((v, i) => {
+            delete_one(v.id);
+        })
+    }
+    return (
         <div className="noti-blur1">
-        <div className="boxnoti">
-            <div className="notifi">Notifications</div>
-            <div className="clear">เคลียร์</div>
-            <div className="circleNo1"></div>
-            <div className="circleNo2"></div>
-            <div className="circleNo3"></div>
-            <div className="circleNo4"></div>
-            <div className="chatNo1"></div>
-            <div className="chatNo2"></div>
-            <div className="chatNo3"></div>
-            <div className="chatNo4"></div>
-            <div className="icon-big" onClick={() => setShow(false)}>
-                <IoMdCloseCircle size={45} />
-            </div>
-            <div className="icon-small1">
-                <IoMdCloseCircle size={25} color='666666' />
-            </div>
-            <div className="icon-small2">
-                <IoMdCloseCircle size={25} color='666666' />
-            </div>
-            <div className="icon-small3">
-                <IoMdCloseCircle size={25} color='666666' />
-            </div>
-            <div className="icon-small4">
-                <IoMdCloseCircle size={25} color='666666' />
-            </div>
-        </div>
+            <div className="boxnoti">
+                <div className='layout-notifi'>
+                    <div className='nofi-flex-rowbox' >
 
-    </div>
+                        <div className="notifi">Notifications</div>
+                        <div className="icon-big" onClick={() => setShow(false)}>
+                            <IoMdCloseCircle size={45} />
+                        </div>
+
+                    </div>
+                    <div className='noti-chat-box'>
+                        {dataNotifi.length > 0 ? dataNotifi.map((v, i) => (
+                            <div className="chatNo1">
+                                <div className='noti-text-box'>
+                                    {v.name} : {v.info}
+                                </div>
+                                <div className="icon-small1" onClick={() => { delete_one(v.id) }}>
+                                    <IoMdCloseCircle size={25} color='666666' />
+                                </div>
+                            </div>
+                        )
+                        ) : <a style={{ fontSize: "20px" }}>ไม่พบ</a>}
+
+
+                    </div>
+                    <div className='nofi-layout-clear-box'>
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            <div className="clear" onClick={()=>delete_all()}>เคลียร์</div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
     )
 }
 
